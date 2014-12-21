b557c344a59c162ae533f16a41b042d8306632be
Carlos Santos <carlosricardosantos@gmail.com>
2014-12-21 13:05:57 +0000

    Truncate filter added

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -61,3 +61,22 @@ app.controller('DataCtrl', function($scope, $http) {
 
 });
 
+app.filter('truncate', function (){
+  return function (text, length, end){
+    if (text !== undefined){
+      if (isNaN(length)){
+        length = 10;
+      }
+
+      if (end === undefined){
+        end = "...";
+      }
+
+      if (text.length <= length || text.length - end.length <= length){
+        return text;
+      }else{
+        return String(text).substring(0, length - end.length) + end;
+      }
+    }
+  };
+});
